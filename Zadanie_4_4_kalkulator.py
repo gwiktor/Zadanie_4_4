ba85cc37c859478e2f90c3b323683fb4284794db
Wiktor Gabrys <gabrys.wiktor@gmail.com>
2020-05-24 18:27:48 +0200

    Modify Zadanie_4_4_kalkulator

--- Zadanie_4_4_kalkulator.py
+++ Zadanie_4_4_kalkulator.py
@@ -26,31 +26,29 @@ def mnożenie (*args):
   logging.debug(f"Iloczyn liczb wynosi: {iloczyn}")
   return iloczyn
 
+#Zdefiniowanie wywołań
+dzialania = {
+  1: dodawanie,
+  2: odejmowanie,
+  3: mnożenie,
+  4: dzielenie
+}
+
 #Funkcja wyboru działania
 def wybor_dzialania(dzialanie):
-  
   if dzialanie == 1 or dzialanie == 3:
     a = list(map(int, input("Wprowadz liczby oddzielając spacją: ").split()))
-    if dzialanie == 1:
-      dodawanie(*a)
-    elif dzialanie == 3:
-      mnożenie(*a)
-   
+    wynik = dzialania[dzialanie](*a)
   elif dzialanie == 2 or dzialanie == 4:
     a = int(input("Podaj pierwszy składnik: "))
     b = int(input("Podaj drugi składnik: "))
-    if dzialanie == 2:
-      odejmowanie(a, b)
-    elif dzialanie == 4:
-      dzielenie(a, b)
-  
+    wynik = dzialania[dzialanie](a, b)
   else:
     logging.debug("Wybrałeś niepoprawne działanie")
-  
+
 #Wywołanie programu
 if __name__ == "__main__":
   while True:
     dzialanie = int(input("Podaj działanie, posługjąc się odpowiednią liczbą: \n1 - dodawanie \n2 - odejmowanie \n3 - mnożenie \n4 - dzielenie"))
     logging.debug(f"Zostało wywołane działanie numer {dzialanie}")
     wybor_dzialania(dzialanie)
-
