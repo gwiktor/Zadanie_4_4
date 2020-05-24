a8e6d2b6138e7a067fe1801a84f4e5074fc97be5
Wiktor Gabrys <gabrys.wiktor@gmail.com>
2020-05-24 18:09:53 +0200

    Modify Zadanie_4_4_kalkulator

--- Zadanie_4_4_kalkulator.py
+++ Zadanie_4_4_kalkulator.py
@@ -8,15 +8,17 @@ print("Witam w kalkulatorze Wiktora! \n")
 
 #Funkcje poszczególnych działań
 
-def dodawanie(*args):
+def dodawanie (*args):
   suma = sum(args)
   logging.debug(f"Suma liczb wynosi: {suma}")
   return suma
 
 def odejmowanie (a,b):
+  logging.debug(f"Różnica liczb wynosi: {a - b}")
   return a - b
 
 def dzielenie (a,b):
+  logging.debug(f"Dzielenie liczb wynosi: {a / b}")
   return a / b
 
 def mnożenie (*args):
@@ -27,40 +29,28 @@ def mnożenie (*args):
 #Funkcja wyboru działania
 def wybor_dzialania(dzialanie):
   
-  if dzialanie == 1:
+  if dzialanie == 1 or dzialanie == 3:
     a = list(map(int, input("Wprowadz liczby oddzielając spacją: ").split()))
-    dodawanie(*a)
+    if dzialanie == 1:
+      dodawanie(*a)
+    elif dzialanie == 3:
+      mnożenie(*a)
    
-  elif dzialanie == 2:
-    try:
-      while True:
-        a = float(input("Podaj składnik 1: "))
-        b = float(input("Podaj składnik 2: "))
-        logging.debug(f"Odejmuję {b} od {a} \nWynik to {odejmowanie(a, b)}")
-        odejmowanie(a, b)
-    except ValueError:
-      print("Nie wprowadzono liczby")
-  
-  elif dzialanie == 3:
-    a = list(map(int, input("Wprowadz liczby oddzielając spacją: ").split()))
-    mnożenie(*a)
-
-  elif dzialanie == 4:
-    try:
-      while True:
-        a = float(input("Podaj składnik 1: "))
-        b = float(input("Podaj składnik 2: "))
-        logging.debug(f"Dzielę {a} przez {b} \nWynik to {dzielenie(a, b)}")
-        dzielenie(a,b)
-    except ValueError:
-      print("Nie wprowadzono liczby")
+  elif dzialanie == 2 or dzialanie == 4:
+    a = int(input("Podaj pierwszy składnik: "))
+    b = int(input("Podaj drugi składnik: "))
+    if dzialanie == 2:
+      odejmowanie(a, b)
+    elif dzialanie == 4:
+      dzielenie(a, b)
   
   else:
     logging.debug("Wybrałeś niepoprawne działanie")
   
 #Wywołanie programu
 if __name__ == "__main__":
-  dzialanie = int(input("Podaj działanie, posługjąc się odpowiednią liczbą: \n1 - dodawanie \n2 - odejmowanie \n3 - mnożenie \n4 - dzielenie"))
-  logging.debug(f"Zostało wywołane działanie numer {dzialanie}")
-  wybor_dzialania(dzialanie)
+  while True:
+    dzialanie = int(input("Podaj działanie, posługjąc się odpowiednią liczbą: \n1 - dodawanie \n2 - odejmowanie \n3 - mnożenie \n4 - dzielenie"))
+    logging.debug(f"Zostało wywołane działanie numer {dzialanie}")
+    wybor_dzialania(dzialanie)
 
